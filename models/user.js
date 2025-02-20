63fb82e4987527f203695a42be78eff66c820797
antoinebnm <ab20040218@gmail.com>
2025-02-20 16:00:02 +0100

    update User Model

--- models/user.js
+++ models/user.js
@@ -15,7 +15,6 @@ const userSchema = new Schema(
       password: { type: String, required: true },
     },
     addedAt: { type: Date, required: true },
-    gamesPlayed: [{ type: Schema.Types.ObjectId, ref: "Game" }],
   },
   { versionKey: false }
 );
